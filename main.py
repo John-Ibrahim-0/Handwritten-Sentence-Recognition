6a8c8fde149007ff38e87fb4f7ce1ce662fdcae0
John Ibrahim <john.ibrahim119@gmail.com>
2025-12-03 18:52:23 -0500

    feat: added data transforms and visualization

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import os
 from tqdm import tqdm
+from PIL import Image
 
 import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
+import matplotlib.pyplot as plt
+
 from sklearn.model_selection import train_test_split
 
 from dataset import HandwrittenSentenceDataset
 
+from utils.visualization import show_before_after
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Device: {device}")
+print(f"Device: {device}\n")
 
 ## Load data
 
@@ -32,7 +37,7 @@ with open(LABEL_PATH, "r") as file:
             continue
 
         image_folder_1 = line[0][:3]
-        image_folder_2 = line[0][3:8].rstrip("-")
+        image_folder_2 = line[0][:8].rstrip("-")
         image_file_name = line[0] + ".png"
 
         image_path = os.path.join(IMAGE_FOLDER_PATH, image_folder_1, image_folder_2, image_file_name)
@@ -53,13 +58,54 @@ images_val, images_test, labels_val, labels_test = train_test_split(images_temp,
 
 print("| Data split.")
 
+## Create transforms
+
+train_transforms = transforms.Compose([
+    transforms.Grayscale(num_output_channels=1),
+
+    transforms.Resize(32),
+
+    transforms.RandomApply([
+        transforms.RandomAffine(degrees=3, translate=(0.03, 0.03), scale=(0.95, 1.05), shear=3)], p=0.7
+    ),
+
+    transforms.RandomPerspective(distortion_scale=0.05, p=0.3),
+
+    transforms.RandomApply([
+        transforms.ColorJitter(brightness=0.2, contrast=0.2)], p=0.5
+    ),
+
+    transforms.RandomApply([
+        transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 0.6))], p=0.2
+    ),
+
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5], std=[0.5])
+])
+
+val_test_transforms = transforms.Compose([
+    transforms.Grayscale(num_output_channels=1),
+    transforms.Resize(32),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5], std=[0.5])
+])
+
+## Visualize transforms
+
+image_path = images_train[0]
+
+raw_image = Image.open(image_path).convert("L")
+transformed_image = train_transforms(raw_image)
+
+show_before_after(raw_image, transformed_image)
+
 ## Create datasets
 
 print("\nCreating datasets:")
 
-train_dataset = HandwrittenSentenceDataset(images_train, labels_train)
-val_dataset = HandwrittenSentenceDataset(images_val, labels_val)
-test_dataset = HandwrittenSentenceDataset(images_test, labels_test)
+train_dataset = HandwrittenSentenceDataset(images_train, labels_train, train_transforms)
+val_dataset = HandwrittenSentenceDataset(images_val, labels_val, val_test_transforms)
+test_dataset = HandwrittenSentenceDataset(images_test, labels_test, val_test_transforms)
 
 print("| Datasets ceated.")
 
