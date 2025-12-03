1de7801e151c49b98b9ee5905dfddcb399c1baa7
John Ibrahim <john.ibrahim119@gmail.com>
2025-12-03 18:51:52 -0500

    feat: added transform visualization before and after

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,2 +1,22 @@
+import matplotlib.pyplot as plt
+
 def unnormalize(tensor):
     return tensor * 0.5 + 0.5
+
+def show_before_after(raw_image, transformed_image):
+    plt.figure(figsize=(20, 4))
+
+    # raw image
+    plt.subplot(1, 2, 1)
+    plt.title("Before Transforms")
+    plt.imshow(raw_image, cmap="gray")
+    plt.axis("off")
+
+    # transformed image
+    plt.subplot(1, 2, 2)
+    plt.title("After Transforms")
+    image = unnormalize(transformed_image).permute(1, 2, 0).squeeze() # (C, H, W) -> (H, W, C) -> (H, W)
+    plt.imshow(image, cmap="gray")
+    plt.axis("off")
+
+    plt.show()
