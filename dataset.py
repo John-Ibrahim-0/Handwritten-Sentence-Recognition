a31e5fc55f9398139cfebb5ec1fd2d971b89cb15
John Ibrahim <john.ibrahim119@gmail.com>
2025-12-03 19:38:14 -0500

    feat: added label encoding and CTC decoding

--- dataset.py
+++ dataset.py
@@ -1,11 +1,29 @@
 from PIL import Image
 from torch.utils.data import Dataset
 
+from configs import Configs
+
 class HandwrittenSentenceDataset(Dataset):
-    def __init__(self, image_paths, labels, transforms=None):
+    def __init__(self, image_paths, labels, vocab, transforms=None):
         self.image_paths = image_paths
         self.labels = labels
+        self.vocab = vocab
         self.transforms = transforms
+
+        configs = Configs()
+        BLANK = configs.BLANK_LABEL
+        
+        self.char2idx = {BLANK: 0}
+        for i, char in enumerate(vocab, start=1):
+            self.char2idx[char] = i
+        
+        self.idx2char = {i: char for char, i in self.char2idx.items()}
+    
+    def encode_label(self, label):
+        return [self.char2idx[char] for char in label if char in self.char2idx]
+
+    def decode_sequence(self, seq):
+        return "".join([self.idx2char[i] for i in seq if i in self.idx2char])
     
     def __len__(self):
         return len(self.image_paths)
@@ -16,6 +34,6 @@ class HandwrittenSentenceDataset(Dataset):
         if self.transforms:
             image = self.transforms(image)
         
-        label = self.labels[index]
+        label = self.encode_label(self.labels[index])
 
         return image, label
