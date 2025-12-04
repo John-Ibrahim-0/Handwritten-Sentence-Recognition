52bec096c40823c8ddc01bbbbfc0720295324980
John Ibrahim <john.ibrahim119@gmail.com>
2025-12-03 19:37:38 -0500

    added configs file

---
+++ configs.py
@@ -0,0 +1,20 @@
+import torch
+
+class Configs():
+    # device
+    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+    # training hyperparameters
+    BATCH_SIZE = 32
+    LEARNING_RATE = 0.001
+    EPOCHS = 1000
+
+    # model hyperparameters
+    IMG_HEIGHT = 32
+
+    # CTC parameters
+    BLANK_LABEL = "-"
+
+    # paths
+    DATA_FOLDER = "data/sentences"
+    LABEL_FILE = "data/ascii/sentences.txt"
